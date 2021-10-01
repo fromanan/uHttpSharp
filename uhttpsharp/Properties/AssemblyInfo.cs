10071c2b0cd0ddef045fd0fd3a9efe11202ca0aa
Tony Froman <fromanan@starforged.net>
2021-10-01 17:38:10 -0400

    Added public key for strongly signed assembly

--- uhttpsharp/Properties/AssemblyInfo.cs
+++ uhttpsharp/Properties/AssemblyInfo.cs
@@ -30,4 +30,8 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("2a229a48-3435-4e8d-bfda-9e2535940a81")]
 
-[assembly:InternalsVisibleTo("uhttpsharp.Tests")]
+[assembly:InternalsVisibleTo("uhttpsharp.Tests,PublicKey="+
+                             "002400000480000094000000060200000024000052534131000400000100010099677ceded0cb77ef1480d30d7eda" +
+                             "923bfc0b103491e5b3b030c3cf7cda2ce60b4b8b129af9b694aff75013bfe3afae27a1e999e62a1381eed3953a1a8" +
+                             "6cd38e1766efcf9e5b4b4a96961427709829b4ff0c2f8e8c1838e0e83d9519d27939dc9b8327fdc8006fae8fec9cb" +
+                             "f63cf8c5d0ec2c2a458d5d6d289654fb2f7beb1d2")]
